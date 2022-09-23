5d5172fbb844f8bc98595e2c233c8aedce68fd2e
Zidri <zidrathan@gmail.com>
2022-09-23 14:47:38 -0600

    update

--- randScript.js
+++ randScript.js
@@ -1,18 +1,23 @@
 
 var $min = 0;
-var $max = 0;
+var $max = 100;
 var $rand = 0;
+var $guessCnt = 5;
+var $gameStart = false;
+
 
 function randGen() {
 
+    $gameStart = true;
 
+    // resetGame();
 
-    while ($min >= $max) {
-        $min = Math.floor(Math.random() * (100 + 1));
-        $max = Math.floor(Math.random() * (100 + 1));
-    }
+    //start with random range
+    // while ($min >= $max) {
+    //     $min = Math.floor(Math.random() * (100 + 1));
+    //     $max = Math.floor(Math.random() * (100 + 1));
+    // }
 
-    console.log($min + " < " + $max);
 
     if ($min != null && $max != null && $min < $max) {
 
@@ -32,33 +37,101 @@ function randGen() {
 }
 
 function guessCheck() {
-    var $guess = document.forms["guessForm"]["guessNumber"].value;
+    if ($gameStart) {
+        var $guess = document.forms["guessForm"]["guessNumber"].value;
 
-    console.log("Your guess: "+$guess);
+        console.log("min: " + $min + " max: " + $max);
+        console.log("Answer: " + $rand);
+        console.log("Your guess: " + $guess + " Cnt: " + $guessCnt);
 
-    if ($guess != null && $guess > $min && $guess < $max){
-        if($guess > $rand){
-            //update range
 
-            //guess lower
+        if ($guess > $min && $guess < $max) {
+            if ($guess > $rand) {
+                //update range
+                $max = $guess;
+                document.getElementById("rangeMax").innerHTML = $guess;
+                //guess lower
+                document.getElementById("guessWarning").innerHTML = "Too high! Guess Lower";
+                //up guess count
+                if ($guessCnt > 1) {
 
-            //up guess count
+                    document.getElementById("life" + $guessCnt).style.color = "#85706E";
+                    $guessCnt--;
+                    console.log("Cnt: " + $guessCnt);
+                }
+                else if ($guessCnt == 1) {
+                    //remove last life
+                    document.getElementById("life" + $guessCnt).style.color = "#85706E";
+
+                    //display $rand
+                    document.getElementById("randNumber").style.display = "block";
+                    
+                    //You lose! play again? (display guess count)
+                    document.getElementById("guessWarning").innerHTML = "You lost!";
+                    document.getElementById("startBtn").style.display = "block";
+                    document.getElementById("guessBtn").style.display = "none";
+                }
+
+            }
+            else if ($guess < $rand) {
+                //update range
+                $min = $guess;
+                document.getElementById("rangeMin").innerHTML = $guess;
+                //guess higher
+                document.getElementById("guessWarning").innerHTML = "Too low! Guess Higher";
+                //up guess count
+                if ($guessCnt > 1) {
+
+                    document.getElementById("life" + $guessCnt).style.color = "#85706E";
+                    $guessCnt--;
+                    console.log("Cnt: " + $guessCnt);
+                }
+                else if ($guessCnt == 1) {
+                    //remove last life
+                    document.getElementById("life" + $guessCnt).style.color = "#85706E";
+
+                    //display $rand
+                    document.getElementById("randNumber").style.display = "block";
+
+                    //You lose! play again? (display guess count)
+                    document.getElementById("guessWarning").innerHTML = "You lost!";
+                    document.getElementById("startBtn").style.display = "block";
+                    document.getElementById("guessBtn").style.display = "none";
+                }
+            }
+            else if ($guess == $rand) {
+                //display $rand
+                document.getElementById("randNumber").style.display = "block";
+                //You win! play again? (display guess count)
+                document.getElementById("guessWarning").innerHTML = "You got it!";
+                document.getElementById("startBtn").style.display = "block";
+
+                document.getElementById("guessBtn").style.display = "none";
+            }
         }
-        else if($guess < $rand){
-            //update range
-
-            //guess higher
-
-            //up guess count
-        }
-        else if($guess === $rand){
-            //display $rand
-
-            //You win! play again? (display guess count)
+        else{
+            //number not in range
+            document.getElementById("guessWarning").innerHTML = $guess + " is not in the given range";
         }
     }
-    else{
-        //number not in range
-        document.getElementsByClassName("guessWarning").innerHTML = "Your guess is not in the given range";
+    else {
+        randGen();
     }
+
+}
+
+function resetGame() {
+    for ($guessCnt = 1; $guessCnt < 6; $guessCnt++) {
+        document.getElementById("life" + $guessCnt).style.color = "#B71515";
+    }
+    $guessCnt = 5;
+    $min = 0;
+    $max = 100;
+    document.getElementById("randNumber").style.display = "none";
+    document.getElementById("guessWarning").innerHTML = "";
+    document.getElementById("startBtn").style.display = "none";
+    document.getElementById("guessBtn").style.display = "block";
+    document.forms["guessForm"]["guessNumber"].value = "";
+
+    randGen();
 }
